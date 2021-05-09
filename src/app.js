5e1ab67345670f38e4521ab7d481b1f610594c44
unknown <vimalkuriensam@gmail.com>
2021-05-09 11:45:38 +0530

    init commit

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./styles/main.scss";
+
+const connectedComponents = (
+  <div>
+    <h1>BoilerPlate</h1>
+  </div>
+);
+
+ReactDOM.render(connectedComponents, document.getElementById("app"));
