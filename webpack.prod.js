68e5167386dc31de44ffb3665bafc0ede69ce14e
unknown <vimalkuriensam@gmail.com>
2021-05-11 21:24:51 +0530

    prod webpack changes

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,8 +1,22 @@
-  
+const path = require("path");
+const webpack = require("webpack");
+const dotenv = require("dotenv");
+
 const { merge } = require("webpack-merge");
 const common = require("./webpack.common.js");
 
-module.exports = merge(common, {
-  mode: "production",
-  devtool: "source-map",
-});
+module.exports = (env) => {
+  const fileEnv = dotenv.config({
+    path: path.join(__dirname, `/src/environment/.env.${env}`),
+  }).parsed;
+  console.log(fileEnv);
+  const envKeys = Object.keys(fileEnv).reduce((prev, next) => {
+    prev[`process.env.${next}`] = JSON.stringify(fileEnv[next]);
+    return prev;
+  }, {});
+  return merge(common, {
+    mode: "production",
+    devtool: "source-map",
+    plugins: [new webpack.DefinePlugin(envKeys)],
+  });
+};
